6505e87ae77a1e0f99934a4c5152b41313d9117f
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-10-06 00:35:48 +0530

    Fragment changes

--- CriminalIntent/src/com/bignerdranch/android/criminalIntent/CrimeActivity.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalIntent/CrimeActivity.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.criminalIntent;
 
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +14,16 @@ public class CrimeActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_crime);
+		FragmentManager fm = getSupportFragmentManager();
+		Fragment fragment = fm.findFragmentById(R.id.fragmentContainer);
+		if (fragment == null) {
+		fragment = new CrimeFragment();
+		fm.beginTransaction()
+		.add(R.id.fragmentContainer, fragment)
+		.commit();
+		}
+
+
 	}
 
 	
