7ae2142b618b9c9012d366b22e52056e96d2329c
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-10-05 23:36:20 +0530

    Criminal Fragments added

--- CriminalIntent/src/com/bignerdranch/android/criminalIntent/CrimeActivity.java
+++ CriminalIntent/src/com/bignerdranch/android/criminalIntent/CrimeActivity.java
@@ -21,9 +21,6 @@ public class CrimeActivity extends FragmentActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
 		return super.onOptionsItemSelected(item);
 	}
 }
